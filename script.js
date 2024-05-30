eacf0fd07c8b73a0ff7368ef5b93f63d425e0a40
Farheen123 <farheenirfan12nov@gmail.com>
2024-05-30 00:50:54 -0700

    javascript

---
+++ script.js
@@ -0,0 +1,18 @@
+
+//radius=prompt("enter radius")
+//force=prompt("enter force")
+
+//console.log("The torque is" , radius*force , "N-m")
+
+
+//Energy= prompt("enter K.E")
+//mass= prompt("enter mass")
+//velocity=prompt("enter velocity")
+
+//console.log("The kinetic energy is" , Energy- mass * velocity)
+
+workdone=prompt("enter w")
+Time=prompt("Enter time")
+
+document.write("the power of volt is " , workdone / Time , "Watt")
+
